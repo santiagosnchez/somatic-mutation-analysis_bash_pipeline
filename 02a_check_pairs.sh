1f14c580f2a07c12e9aca9d03d16962d90b70b6a
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2022-02-11 12:48:21 -0500

    updated 02a

--- 02a_check_pairs.sh
+++ 02a_check_pairs.sh
@@ -15,18 +15,31 @@ cd $PBS_O_WORKDIR
 # print jobid to 1st line
 echo $PBS_JOBID
 
-#
+# make directories
+# create log dir
+if [[ ! -e all_logfiles ]]; then
+    mkdir all_logfiles
+fi
+
+# create tmp dir
+if [[ ! -e tmp ]]; then
+    mkdir tmp
+fi
+
+# check length of file names and run script
 if [[ ${#forward} -gt 0 && ${#reverse} -gt 0 ]]; then
+   # run script
    ${pipeline_dir}/fetch_fwd_rev_sing.pl ${forward} ${reverse} | tee -a main.log
    if [[ "$?" == 0 ]]; then
+       # get total singletons
        total_single=$(cat ${sample}.checkpairs.log | grep "Single: " | sed 's/.*: //')
        if [[ "${total_single}" -gt 0 ]]; then
-          new_forward=$(cat ${sample}.checkpairs.log | grep "tmp/.*.1.fastq.gz")
-          new_reverse=$(cat ${sample}.checkpairs.log | grep "tmp/.*.2.fastq.gz")
-          singletons=$(cat ${sample}.checkpairs.log | grep "tmp/.*.S.fastq.gz")
+          new_forward=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.1.fastq.gz")
+          new_reverse=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.2.fastq.gz")
+          singletons=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.S.fastq.gz")
           # make tmp file_list file
           echo "${sample},${new_forward},${new_reverse}" >> tmp/${sample}_file_list.csv
-          echo "${sample},${singletons}" >> tmp/${sample}_file_list.csv
+          echo "${sample},${singletons}," >> tmp/${sample}_file_list.csv
           # calculate new walltime
           wt=$(get_walltime $new_forward $new_reverse)
           # submit new jobs
@@ -35,13 +48,26 @@ if [[ ${#forward} -gt 0 && ${#reverse} -gt 0 ]]; then
           wt=$(get_walltime $singletons)
           qsub -l walltime="${wt}":00:00 -v file_list="tmp/${sample}_file_list.csv",index="${index}s",sample=${sample},rg="${rg}",forward=${singletons},reverse="",mode=${mode} ${pipeline_dir}/02b_align_and_sort_bam_to_ref.bwa.sh
       else
+          # delete tmp files
+          new_forward=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.1.fastq.gz")
+          new_reverse=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.2.fastq.gz")
+          singletons=$(cat ${sample}.checkpairs.log | grep "tmp/${sample}.*.S.fastq.gz")
+          rm $new_forward $new_reverse $singletons
           # proceed normally
           qsub -l walltime="${wt}":00:00 -v file_list="$file_list",index=${index},sample=${sample},rg="${rg}",forward=${forward},reverse=${reverse},mode=${mode} ${pipeline_dir}/02b_align_and_sort_bam_to_ref.bwa.sh
       fi
    else
       echo "fetch_fwd_rev_sing.pl failed with an error for ${sample}"
+      exit 1
    fi
 else
     # submit as single ended
      qsub -l walltime="${wt}":00:00 -v file_list="$file_list",index=${index},sample=${sample},rg="${rg}",forward=${forward},reverse="",mode=${mode} ${pipeline_dir}/02b_align_and_sort_bam_to_ref.bwa.sh
+fi
+
+# final check
+if [[ "$?" == 0 ]]; then
+    # move log
+    mv ${sample}.checkpairs.log all_logfiles
+    echo "02: Done checking pairs for ${sample}" | tee -a main.log
 fi
