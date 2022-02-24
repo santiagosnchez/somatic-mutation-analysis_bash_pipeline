07e90c79989c2c8fb7d6cab79fa97af7b6b98a0d
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2022-02-24 18:13:30 -0500

    get snpeff annotations (working)

--- snpeff-vcf2tbl.sh
+++ snpeff-vcf2tbl.sh
@@ -4,18 +4,24 @@ vcf_file=$1
 bcftools view $vcf_file | \
 perl -ne '
   if ($_ =~ /^#/){
-    if ($_ =~ m/##Funcotator /){
-      print $_;
-    }
     if ($_ =~ m/##INFO=<ID=ANN/){
-      $_ =~ m/Functional annotations: (.+?) \">/;
-      @heads = split /\ | /, $1;
-      print join("\t", @heads) . "\n";
+      $_ =~ m/Functional annotations: (.+?)\">/;
+      @heads = split / \| /, $1;
+      $str_head = join(",", @heads);
+      print "Chromosome,Position,Reference," . substr($str_head, 1, length($str_head)-2) . "\n";
     }
   } else {
     @fields = split /\t/, $_;
-    $fields[7] =~ m/;*ANN=(.+?);*/;
-    @maf = split /\|/, $1;
-    print join("\t", @maf) . "\n";
+    @annfields = split /;/, $fields[7];
+    %annfieldsmap = {};
+    foreach (@annfields){
+      @lab_ann = split /=/, $_;
+      $annfieldsmap{$lab_ann[0]} = $lab_ann[1];
+    }
+    @annsubfields = split(/,/, $annfieldsmap{"ANN"});
+    foreach (@annsubfields){
+       $_ =~ s/\|/,/g;
+       print join(",", @fields[0,1,3]) . "," . $_ . "\n";
+    }
   }
-' | sed "s/\'//g"
+'
