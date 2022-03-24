66446efacb0e640016cc3163542434ac440afb46
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2022-03-24 19:24:48 -0400

    missing -a in tee cmd

--- 01_submit_jobs.sh
+++ 01_submit_jobs.sh
@@ -161,7 +161,7 @@ elif [[ ${mode} == "wes" || ${mode} == "wgs" ]]; then
             # add date and mode to main.log
             date | tee main.log
         fi
-        echo -e "\n01: Running as mode: ${mode}" | tee main.log
+        echo -e "\n01: Running as mode: ${mode}" | tee -a main.log
     else
         # print date and mode
         date
