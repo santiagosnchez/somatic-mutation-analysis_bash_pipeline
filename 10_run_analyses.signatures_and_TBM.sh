0dcaa01dc4bb713eeb4a060f758f7b00e9cde224
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2022-03-30 16:29:45 -0400

    changed export_results

--- 10_run_analyses.signatures_and_TBM.sh
+++ 10_run_analyses.signatures_and_TBM.sh
@@ -244,7 +244,7 @@ if [[ "$check_finish" == 0 ]]; then
 
         # export to zip file
         today=$(data -I)
-        zip -r export_results_${today}.zip analyses/old_output* analyses/mmr_annotations_*
+        zip -r export_results.${today}.zip analyses/old_output* analyses/mmr_annotations_*
 
         # add tmb_and_coverage to archive
         #zip -ru analyses.zip analyses/coverage_and_tmb.csv
