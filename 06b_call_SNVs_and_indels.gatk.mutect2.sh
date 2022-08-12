fc903a2d87194a0edff761f5f2334ddb9dd04b6d
Santiago Sanchez-Ramirez <santiago.snchez@gmail.com>
2022-08-12 00:23:16 -0400

    Mutect2 runs without PoN of it does not exist

--- 06b_call_SNVs_and_indels.gatk.mutect2.sh
+++ 06b_call_SNVs_and_indels.gatk.mutect2.sh
@@ -88,7 +88,7 @@ $gatk_path/gatk --java-options "-Xmx20G -Djava.io.tmpdir=./.tmp" Mutect2 \
  --genotype-germline-sites true \
  --genotype-pon-sites true \
  -L ${bed30intervals}/${bed}
-  elif [[ ${gnomad_resource} == 'null' ]]; then
+    elif [[ ${gnomad_resource} == 'null' && -e ${gatk_pon} ]]; then
 $gatk_path/gatk --java-options "-Xmx20G -Djava.io.tmpdir=./.tmp" Mutect2 \
  -I ${dir}/${tumor}.bqsr.bam \
  -I ${dir}/${normal}.bqsr.bam \
@@ -101,6 +101,18 @@ $gatk_path/gatk --java-options "-Xmx20G -Djava.io.tmpdir=./.tmp" Mutect2 \
  --f1r2-tar-gz mutect2/f1r2/${tumor}__${normal}.${index}.f1r2.tar.gz \
  --genotype-germline-sites true \
  --genotype-pon-sites true \
+ -L ${bed30intervals}/${bed}
+    elif [[ ${gnomad_resource} == 'null' && ! -e ${gatk_pon} ]]; then
+$gatk_path/gatk --java-options "-Xmx20G -Djava.io.tmpdir=./.tmp" Mutect2 \
+ -I ${dir}/${tumor}.bqsr.bam \
+ -I ${dir}/${normal}.bqsr.bam \
+ -tumor ${tumor} \
+ -normal ${normal} \
+ -R ${reference} \
+ -O mutect2/${tumor}__${normal}.mutect2.unfiltered.${mode}.${index}.vcf \
+ --max-mnp-distance 0 \
+ --f1r2-tar-gz mutect2/f1r2/${tumor}__${normal}.${index}.f1r2.tar.gz \
+ --genotype-germline-sites true \
  -L ${bed30intervals}/${bed}
     else
 # submit GetPileupSummaries for normal and tumor
